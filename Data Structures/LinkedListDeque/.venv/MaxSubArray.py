4323a9f5225f18099e2c5ac6f7aaea2c341f5281
AEToluca <aeestrada1004@gmail.com>
2025-03-31 14:54:12 -0400

    Added maxsubarray file

---
+++ Data Structures/LinkedListDeque/.venv/MaxSubArray.py
@@ -0,0 +1,11 @@
+
+def main():
+    k = int(input())
+    int_list = input()
+    array = [int(x) for x in int_list.split(', ')]
+    # Your code goes here
+    # Remember to use the deque you implemented!
+
+
+if __name__ == "__main__":
+    main()
