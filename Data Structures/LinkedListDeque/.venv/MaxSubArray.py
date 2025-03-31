4c78f2b8387845ca6ed456479b1bf998ff4c8678
AEToluca <aeestrada1004@gmail.com>
2025-03-31 15:57:31 -0400

    1st attempt implementation of maxsubarray

--- Data Structures/LinkedListDeque/.venv/MaxSubArray.py
+++ Data Structures/LinkedListDeque/.venv/MaxSubArray.py
@@ -1,11 +1,12 @@
-
+import Deque
 def main():
     k = int(input())
     int_list = input()
     array = [int(x) for x in int_list.split(', ')]
     # Your code goes here
     # Remember to use the deque you implemented!
-
+def maxSubarrayWithRotation(array: list[int], k: int) -> int:
+    pass
 
 if __name__ == "__main__":
     main()
